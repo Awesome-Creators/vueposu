c79c17045fe01e1981253be6090ab6d3430e21b0
yuxino <nbsawz@gmail.com>
2020-08-24 15:34:05 +0800

    feat: add useToogle

--- src/Test.vue
+++ src/Test.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>hello {{text}}</div>
+  <div>
+    hello {{text}}
+    <span>{{String(status)}}</span>
+  </div>
 </template>
 
 <script>
+import useToggle from "./useToggle";
+
 export default {
+  setup() {
+    const [status, toggle] = useToggle(false);
+    return { status, toggle };
+  },
   data() {
     return {
       text: "test",
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -6,4 +6,12 @@ describe("Test.vue", () => {
     const wrapper = shallowMount(Test);
     expect(wrapper.find("div").text()).toMatch("hello test");
   });
+
+  it("test toggle", async () => {
+    const wrapper = shallowMount(Test);
+    expect(wrapper.find("span").text()).toMatch("false");
+    (wrapper.vm as any).toggle();
+    await wrapper.vm.$nextTick();
+    expect(wrapper.find("span").text()).toMatch("true");
+  });
 });
