82b41403dc28bd7263dedfea5686dfdd0a7bde30
glitchboyl <lonelyliar8523@gmail.com>
2020-08-24 23:02:45 +0800

    feat: useToggle types definition

--- src/useToggle.ts
+++ src/useToggle.ts
@@ -1,29 +1,54 @@
-import { ref } from 'vue';
+import { ref, Ref, UnwrapRef } from 'vue';
+
+type IState = string | number | boolean | null | undefined;
+
+interface IActions<T = IState> {
+  setLeft: () => T;
+  setRight: () => T;
+  toggle: (value: T) => T;
+}
+
+function useToggle<D = boolean | undefined>(): [
+  Ref<UnwrapRef<boolean>>,
+  IActions<D>,
+];
+
+function useToggle<D = IState>(
+  defaultValue: D,
+): [Ref<UnwrapRef<D>>, IActions<D>];
+
+function useToggle<D = IState, R = IState>(
+  defaultValue: D,
+  reverseValue: R,
+): [Ref<UnwrapRef<D | R>>, IActions<D | R>];
 
 /**
  * useToggle function
- * @param leftValue  Truth value, default is true  - an optional parameter
- * @param rightValue False value, default is false - an optional parameter
+ *
+ * @param defaultValue Truth value, default is true  - an optional parameter
+ * @param reverseValue False value, default is false - an optional parameter
  * @returns [ status, { toggle, setLeft, setRight } ]
-  ]
  */
-export default function useToggle(leftValue?: any, rightValue?: any) {
-  leftValue = leftValue === void 0 ? true : leftValue;
-  rightValue = rightValue === void 0 ? !Boolean(leftValue) : rightValue;
+function useToggle<D extends IState = IState, R extends IState = IState>(
+  defaultValue: D = true as D,
+  reverseValue?: R,
+) {
+  defaultValue = (defaultValue === void 0 ? true : defaultValue) as D;
+  reverseValue = (reverseValue === void 0 ? !defaultValue : reverseValue) as R;
 
-  const status = ref(leftValue);
+  const status = ref<D | R>(defaultValue);
 
   const toggle = (value?: any) =>
     (status.value =
       value !== void 0
         ? value
-        : status.value !== leftValue
-        ? leftValue
-        : rightValue);
+        : status.value !== reverseValue
+        ? reverseValue
+        : defaultValue);
 
-  const setLeft = () => (status.value = leftValue);
+  const setLeft = () => (status.value = defaultValue as UnwrapRef<D>);
 
-  const setRight = () => (status.value = rightValue);
+  const setRight = () => (status.value = reverseValue as UnwrapRef<R>);
 
   return [
     status,
@@ -34,3 +59,5 @@ export default function useToggle(leftValue?: any, rightValue?: any) {
     },
   ];
 }
+
+export default useToggle;
