b7d714684818623627e554fa1614f500fa926049
glitchboyl <lonelyliar8523@gmail.com>
2020-09-05 00:52:36 +0800

    feat: lib/isDocumentVisible

---
+++ src/lib/isDocumentVisible.ts
@@ -0,0 +1,12 @@
+function isDocumentVisible(): boolean {
+  if (
+    typeof document !== 'undefined' &&
+    typeof document.visibilityState !== 'undefined'
+  ) {
+    return document.visibilityState !== 'hidden';
+  }
+  // always assume it's visible
+  return true;
+}
+
+export default isDocumentVisible;
