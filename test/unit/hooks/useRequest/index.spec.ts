33cec794fcf0c2023d5781d8c5259fc6c3add5f8
yuxino <nbsawz@gmail.com>
2020-09-04 17:54:56 +0800

    chore: remove useRequest test case

--- test/unit/hooks/useRequest/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils';
-import Test from './test.comp.vue';
-
-describe('useRequest', () => {
-  it('test', () => {
-    const component = mount(Test);
-    component.unmount();
-  });
-});
