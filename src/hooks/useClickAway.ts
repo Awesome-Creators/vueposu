ba48745a8de696933e612bd56ecd8223712b49c9
glitchboyl <lonelyliar8523@gmail.com>
2020-09-04 11:53:25 +0800

    feat: useClickAway

---
+++ src/hooks/useClickAway.ts
@@ -0,0 +1,46 @@
+import { watchEffect } from 'vue';
+import { getTargetElement, Target } from '@lib/dom';
+
+const defaultEvent = 'click';
+
+type EventType = MouseEvent | TouchEvent;
+
+/**
+ * useClickAway function
+ *
+ * @param eventHandler Handler function on external click.
+ * @param target Execution handler target.
+ * @param eventName Event trigger.
+ */
+function useClickAway(
+  eventHandler: (event: EventType) => void,
+  target: Target | Target[],
+  eventName: string = defaultEvent,
+) {
+  const handler = event => {
+    const targets = Array.isArray(target) ? target : [target];
+    const targetElements = targets.map(t =>
+      getTargetElement(t),
+    ) as HTMLElement[];
+
+    if (
+      targetElements.some(
+        targetElement => !targetElement || targetElement.contains(event.target),
+      )
+    ) {
+      return;
+    }
+
+    eventHandler(event);
+  };
+
+  watchEffect(cleanup => {
+    document.addEventListener(eventName, handler);
+
+    cleanup(() => {
+      document.removeEventListener(eventName, handler);
+    });
+  });
+}
+
+export default useClickAway;
