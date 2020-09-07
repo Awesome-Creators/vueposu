0e50392cdaf6ce891982bfe38af4c6e99f0724db
yuxino <nbsawz@gmail.com>
2020-09-07 23:48:09 +0800

    feat: ignore isOnline/isDocumentVisible in test converage

--- src/libs/isDocumentVisible.ts
+++ src/libs/isDocumentVisible.ts
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 export default function isDocumentVisible(): boolean {
   if (
     typeof document !== 'undefined' &&
--- src/libs/isOnline.ts
+++ src/libs/isOnline.ts
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 export default function isOnline(): boolean {
   if (typeof navigator.onLine !== 'undefined') {
     return navigator.onLine;
