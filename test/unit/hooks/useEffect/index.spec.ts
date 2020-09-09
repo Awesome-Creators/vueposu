b9f4e859ded8ebcb5c30a4f4e7de93f8ad26bd01
yuxino <nbsawz@gmail.com>
2020-09-09 21:04:35 +0800

    test: add useEffect test case

--- test/unit/hooks/useEffect/index.spec.ts
+++ test/unit/hooks/useEffect/index.spec.ts
@@ -1,10 +1,45 @@
 import { mount } from '@vue/test-utils';
-import Test from './test.comp.vue';
+import NotDeps from './test.notdeps.comp.vue';
+import Deps from './test.deps.comp.vue';
+import EmptyDeps from './test.emptyDeps.comp.vue';
 
 describe('useEffect', () => {
-  it('test useEffect', async () => {
-    const component = mount(Test);
+  it('test not deps', () => {
+    (window as any).testNotDepsEffect = 1;
+    const component = mount(NotDeps);
+    component.unmount();
+    expect((window as any).testNotDepsEffect).toBe(0);
+  });
+
+  it('test deps', async done => {
+    (window as any).testDepsEffect = 1;
+    (window as any).testDepsSpy = jest.fn();
+
+    const component = mount(Deps);
+
+    await new Promise(res => {
+      setTimeout(() => {
+        res(null);
+        expect((window as any).testDepsEffect).toBe(20);
+      }, 300);
+    });
 
     component.unmount();
+
+    expect((window as any).testDepsEffect).toBe(0);
+    expect((window as any).testDepsSpy).toBeCalledTimes(2);
+    done();
+  });
+
+  it('test empty deps', async done => {
+    (window as any).testEmptyDepsEffect = 1;
+    (window as any).testEmptyDepsSpy = jest.fn();
+
+    const component = mount(EmptyDeps);
+    component.unmount();
+
+    expect((window as any).testEmptyDepsEffect).toBe(0);
+    expect((window as any).testEmptyDepsSpy).toBeCalledTimes(1);
+    done();
   });
 });
---
+++ test/unit/hooks/useEffect/test.notdeps.comp.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>{{val}}</div>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue';
+import useEffect from '@hooks/useEffect';
+
+export default {
+  setup() {
+    const val = ref((window as any).testNotDepsEffect);
+
+    useEffect(() => {
+      return () => {
+        val.value = 1;
+        (window as any).testNotDepsEffect = 0;
+      };
+    });
+
+    return {
+      val,
+    };
+  },
+};
+</script>
