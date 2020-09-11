9b41bf48c6570be7449ac448b2d5d119f824df5f
glitchboyl <lonelyliar8523@gmail.com>
2020-09-11 21:30:37 +0800

    fix: useTitle didn't set the right title

--- src/hooks/useTitle.ts
+++ src/hooks/useTitle.ts
@@ -1,4 +1,5 @@
-import { getCurrentInstance, watchEffect, onUnmounted } from 'vue';
+import { getCurrentInstance } from 'vue';
+import useEffect from './useEffect';
 
 interface ITitleState {
   before: string;
@@ -11,35 +12,38 @@ const titleMap = new Map<number, ITitleState>();
  * set the string to the page title.
  *
  * @param title The string to set to the page title.
+ * @param callback callback function when set the page title.
  */
 
 export default function useTitle(title: string, callback?: () => void) {
   title = String(title);
   const instance = getCurrentInstance();
 
-  if (instance && document.title !== title) {
-    let t = titleMap.get(instance.uid);
+  if (instance) {
+    if (document.title !== title) {
+      let t = titleMap.get(instance.uid);
 
-    if (!t) {
-      titleMap.set(instance.uid, (t = {} as ITitleState));
-    }
+      if (!t) {
+        titleMap.set(instance.uid, (t = {} as ITitleState));
+      }
 
-    if (!t.before) {
-      t.before = document.title;
-    }
+      if (!t.before) {
+        t.before = document.title;
+      }
 
-    if (t.current !== title) {
-      t.current = title;
+      if (t.current !== title) {
+        t.current = title;
 
-      watchEffect(() => {
-        document.title = t.current;
-        callback && callback();
-      });
+        useEffect(() => {
+          document.title = t.current;
+          callback && callback();
 
-      onUnmounted(() => {
-        document.title = t.before;
-        titleMap.delete(instance.uid);
-      });
+          return () => {
+            document.title = t.before;
+            titleMap.delete(instance.uid);
+          };
+        }, [t]);
+      }
     }
   } else {
     throw new Error(
@@ -47,105 +51,3 @@ export default function useTitle(title: string, callback?: () => void) {
     );
   }
 }
-
-// interface ITitleState {
-//   current?: {
-//     title: string;
-//     instance_uid?: number;
-//   };
-//   before?: ITitleState;
-// }
-
-// const defaultTitle = document.title;
-
-// const titleState = shallowRef<ITitleState>({});
-// const currentInstance = shallowRef<{ uid?: number }>({});
-
-// const queue = [];
-// let handling = false;
-
-// function handleQueue() {
-//   handling = true;
-//   setTimeout(() => {
-//     handler();
-//   });
-// }
-
-// function handler() {
-//   handling = false;
-//   if (currentInstance.value.uid) {
-//     if (queue.length > 0) {
-//       if (!titleState.value.current) {
-//         titleState.value.current = {
-//           title: queue[0].before,
-//           instance_uid: currentInstance.value.uid,
-//         };
-//       }
-//       const before = { ...titleState.value };
-//       titleState.value.current = {
-//         title: queue[queue.length - 1].current,
-//         instance_uid: currentInstance.value.uid,
-//       };
-//       titleState.value.before = before;
-//       setTitle(titleState.value.current.title);
-//     }
-//     queue.length = 0;
-//   } else {
-//     queue.length = 0;
-//     throw new Error(
-//       'Invalid hook call. Hooks can only be called inside of `setup()`.',
-//     );
-//   }
-// }
-
-// function setTitle(title: string) {
-//   if (title !== document.title) {
-//     document.title = title;
-//   }
-//   if (title === defaultTitle) {
-//     delete titleState.value.current;
-//     delete titleState.value.before;
-//   }
-// }
-
-// /**
-//  * useTitle function
-//  *
-//  * @param title The string to set to the page title.
-//  */
-// async function useTitle(title: string) {
-//   title = String(title);
-//   const instance = getCurrentInstance();
-
-//   currentInstance.value.uid = instance?.uid;
-
-//   watchEffect(() => {
-//     try {
-//       if (
-//         queue.length > 0 &&
-//         currentInstance.value.uid !== queue[queue.length - 1]?.instance_uid
-//       ) {
-//         handler();
-//       }
-
-//       queue.push({
-//         before: document.title,
-//         current: title,
-//         instance_uid: currentInstance.value.uid,
-//       });
-
-//       if (!handling) {
-//         handleQueue();
-//       }
-//     } catch (err) {}
-//   });
-
-//   if (instance) {
-//     onUnmounted(() => {
-//       if (title === titleState.value?.current?.title) {
-//         titleState.value = titleState.value.before;
-//         setTitle(titleState.value.current.title);
-//       }
-//     });
-//   }
-// }
