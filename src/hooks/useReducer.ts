4358e87e2b30fdf9fd2b2ee10f56f3717265b0d4
glitchboyl <lonelyliar8523@gmail.com>
2020-09-11 17:45:06 +0800

    feat: useReducer

---
+++ src/hooks/useReducer.ts
@@ -0,0 +1,33 @@
+import { computed, ref, ComputedRef, UnwrapRef } from 'vue';
+
+export type Dispatch<A> = (value: A) => void;
+type Reducer<S, A> = (prevState: S, action: A) => S;
+
+/**
+ * An alternative to `useState`.
+ * useReducer just like React-style.
+ *
+ * @param reducer
+ * @param initializerArg
+ * @param initializer
+ */
+function useReducer<R extends Reducer<any, any>, I>(
+  reducer: R,
+  initializerArg: I,
+  initializer?: (arg?: I) => UnwrapRef<I>,
+): [ComputedRef<UnwrapRef<I>>, Dispatch<R>] {
+  const state = ref(initializerArg);
+
+  if (initializer && typeof initializer === 'function') {
+    state.value = initializer(initializerArg);
+  }
+
+  const reducerState = computed(() => state.value);
+
+  return [
+    reducerState,
+    action => (state.value = reducer(reducerState.value, action)),
+  ];
+}
+
+export default useReducer;
