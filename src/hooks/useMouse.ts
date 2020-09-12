a004ed64fadc6f2cd13a37b4a3aea65626d8c5b3
glitchboyl <lonelyliar8523@gmail.com>
2020-09-12 17:35:19 +0800

    feat: useMouse

---
+++ src/hooks/useMouse.ts
@@ -0,0 +1,41 @@
+import useState from './useState';
+import useEffect from './useEffect';
+
+interface IMouseCursorState {
+  pageX: number;
+  pageY: number;
+  screenX: number;
+  screenY: number;
+  clientX: number;
+  clientY: number;
+}
+
+const initState: IMouseCursorState = {
+  pageX: 0,
+  pageY: 0,
+  screenX: 0,
+  screenY: 0,
+  clientX: 0,
+  clientY: 0,
+};
+
+const MOUSE_MOVE = 'mousemove';
+
+export default function useMouse() {
+  const [state, setState] = useState(initState);
+
+  const moveHandler = (event: MouseEvent) => {
+    const { pageX, pageY, screenX, screenY, clientX, clientY } = event;
+    setState({ pageX, pageY, screenX, screenY, clientX, clientY });
+  };
+
+  useEffect(() => {
+    document.addEventListener(MOUSE_MOVE, moveHandler);
+
+    return () => {
+      document.removeEventListener(MOUSE_MOVE, moveHandler);
+    };
+  }, []);
+
+  return state;
+}
