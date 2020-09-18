9e29fa05d6421494cf37b27ba0fed648b7464ca5
glitchboyl <lonelyliar8523@gmail.com>
2020-09-19 02:19:15 +0800

    test: refactor useToggle test case

--- test/unit/hooks/useToggle.spec.ts
+++ test/unit/hooks/useToggle.spec.ts
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils';
 import useToggle from '@hooks/useToggle';
-import { defineComponent } from 'vue-demi';
+import { defineComponent, ref } from 'vue-demi';
 
 describe('hooks/useToggle', () => {
-  it('simple toggle test', async () => {
+  it('should be work when `defaultValue` is undef', async () => {
     const component = mount(
       defineComponent({
         setup() {
@@ -23,26 +23,24 @@ describe('hooks/useToggle', () => {
     component.vm.toggle();
     expect(component.vm.status).toBe(false);
 
+    component.vm.setLeft();
+    expect(component.vm.status).toBe(true);
+
     component.vm.toggle('boy next door');
     expect(component.vm.status).toBe('boy next door');
-
-    component.vm.toggle();
-    expect(component.vm.status).toBe(true);
 
     component.vm.setRight();
     expect(component.vm.status).toBe(false);
 
-    component.vm.setLeft();
+    component.vm.toggle();
     expect(component.vm.status).toBe(true);
-
-    component.unmount();
   });
 
-  it('defaultValue toggle test', async () => {
+  it('should be work when `reverseValue` is undef', async () => {
     const component = mount(
       defineComponent({
         setup() {
-          const [status, toggles] = useToggle('open', 'close');
+          const [status, toggles] = useToggle('hello');
 
           return {
             status,
@@ -53,23 +51,66 @@ describe('hooks/useToggle', () => {
       }),
     );
 
-    expect(component.vm.status).toBe('open');
+    expect(component.vm.status).toBe('hello');
 
     component.vm.toggle();
-    expect(component.vm.status).toBe('close');
+    expect(component.vm.status).toBe(false);
+
+    component.vm.setLeft();
+    expect(component.vm.status).toBe('hello');
 
     component.vm.toggle('boy next door');
     expect(component.vm.status).toBe('boy next door');
 
-    component.vm.toggle();
-    expect(component.vm.status).toBe('open');
-
     component.vm.setRight();
-    expect(component.vm.status).toBe('close');
+    expect(component.vm.status).toBe(false);
 
+    component.vm.toggle();
+    expect(component.vm.status).toBe('hello');
+  });
+
+  it('should be work when `defaultValue`, `reverseValue` is RefTyped', async () => {
+    const component = mount(
+      defineComponent({
+        setup() {
+          const defaultValue = ref('hello');
+          const reverseValue = ref('world');
+          const [status, toggles] = useToggle(defaultValue, reverseValue);
+
+          return {
+            defaultValue,
+            reverseValue,
+            status,
+            ...toggles,
+          };
+        },
+        template: `<template />`,
+      }),
+    );
+
+    expect(component.vm.status).toBe('hello');
+
+    component.vm.toggle();
+    expect(component.vm.status).toBe('world');
+
+    component.vm.toggle('boy next door');
+    expect(component.vm.status).toBe('boy next door');
+    
+    component.vm.toggle();
+    expect(component.vm.status).toBe('hello');
+
+    component.vm.defaultValue = 'boy next door';
     component.vm.setLeft();
-    expect(component.vm.status).toBe('open');
+    expect(component.vm.status).toBe('boy next door');
 
-    component.unmount();
+    component.vm.toggle();
+    expect(component.vm.status).toBe('world');
+
+    component.vm.reverseValue = 'thank you sir';
+    component.vm.setRight();
+    expect(component.vm.status).toBe('thank you sir');
+
+    component.vm.toggle();
+    expect(component.vm.status).toBe('boy next door');
   });
 });
