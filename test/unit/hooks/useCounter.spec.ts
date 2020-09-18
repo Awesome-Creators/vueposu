186d213660ae5ac339e037647ce3d67a01f983ac
glitchboyl <lonelyliar8523@gmail.com>
2020-09-19 02:17:46 +0800

    test: modify useCounter test case

--- test/unit/hooks/useCounter.spec.ts
+++ test/unit/hooks/useCounter.spec.ts
@@ -1,9 +1,82 @@
 import { mount } from '@vue/test-utils';
-import useCounter from '@hooks/useCounter';
 import { defineComponent, ref } from 'vue-demi';
+import useCounter from '@hooks/useCounter';
 
 describe('hooks/useCounter', () => {
-  it('should be work when initialValue, min, max, x is RefTyped.', async () => {
+  it('should be work when it is without options', () => {
+    const component = mount(
+      defineComponent({
+        template: '<template />',
+        setup() {
+          const [count, actions] = useCounter('0');
+
+          return {
+            count,
+            ...actions,
+          };
+        },
+      }),
+    );
+
+    expect(component.vm.count).toBe(0);
+
+    component.vm.inc(2);
+    expect(component.vm.count).toBe(2);
+
+    component.vm.dec();
+    expect(component.vm.count).toBe(1);
+
+    component.vm.set(v => v + 99);
+    expect(component.vm.count).toBe(100);
+
+    component.vm.set(0.2);
+    expect(component.vm.count).toBe(0.2);
+
+    component.vm.inc(0.1);
+    expect(component.vm.count).toBe(0.3);
+
+    component.vm.reset();
+    expect(component.vm.count).toBe(0);
+  });
+
+  
+  it('should be work when it is set options', () => {
+    const component = mount(
+      defineComponent({
+        template: '<template />',
+        setup() {
+          const [count, actions] = useCounter(0, {
+            min: 2,
+            max: '10',
+          });
+
+          return {
+            count,
+            ...actions,
+          };
+        },
+      }),
+    );
+
+    expect(component.vm.count).toBe(2);
+
+    component.vm.inc(2);
+    expect(component.vm.count).toBe(4);
+
+    component.vm.dec();
+    expect(component.vm.count).toBe(3);
+
+    component.vm.set(99);
+    expect(component.vm.count).toBe(10);
+
+    component.vm.set(v => v - 0.2);
+    expect(component.vm.count).toBe(9.8);
+
+    component.vm.reset();
+    expect(component.vm.count).toBe(2);
+  });
+
+  it('should be work when `initialValue`, `min`, `max`, `x` is RefTyped', () => {
     const component = mount(
       defineComponent({
         template: '<template />',
@@ -30,51 +103,51 @@ describe('hooks/useCounter', () => {
       }),
     );
 
-    expect(component.vm.count).toEqual(0.2);
+    expect(component.vm.count).toBe(0.2);
 
     component.vm.inc();
-    expect(component.vm.count).toEqual(0.3);
+    expect(component.vm.count).toBe(0.3);
 
     component.vm.dec(0.2);
-    expect(component.vm.count).toEqual(0.1);
+    expect(component.vm.count).toBe(0.1);
 
     component.vm.set(8);
-    expect(component.vm.count).toEqual(8);
+    expect(component.vm.count).toBe(8);
 
     component.vm.set(v => v + 3);
-    expect(component.vm.count).toEqual(10);
+    expect(component.vm.count).toBe(10);
 
     component.vm.reset();
-    expect(component.vm.count).toEqual(0.2);
+    expect(component.vm.count).toBe(0.2);
 
     component.vm.initial = 2;
     component.vm.reset();
-    expect(component.vm.count).toEqual(2);
+    expect(component.vm.count).toBe(2);
 
     component.vm.min = 3;
     component.vm.reset();
-    expect(component.vm.count).toEqual(3);
+    expect(component.vm.count).toBe(3);
 
     component.vm.max = 4;
     component.vm.x = 2;
     component.vm.inc();
-    expect(component.vm.count).toEqual(4);
-    
+    expect(component.vm.count).toBe(4);
+
     component.vm.dec(4);
-    expect(component.vm.count).toEqual(3);
+    expect(component.vm.count).toBe(3);
 
     component.vm.min = 0;
     component.vm.reset();
-    expect(component.vm.count).toEqual(2);
+    expect(component.vm.count).toBe(2);
 
     // Mock for js users.
     component.vm.initial = 'boy next door' as any;
     component.vm.reset();
-    expect(component.vm.count).toEqual(0);
+    expect(component.vm.count).toBe(0);
 
     component.vm.x = 'thank you sir' as any;
     component.vm.inc();
-    expect(component.vm.count).toEqual(1);
+    expect(component.vm.count).toBe(1);
 
     component.unmount();
   });
