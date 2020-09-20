a7dc3b99081b630a67666a4b72ba44450fbb37a8
yuxino <nbsawz@gmail.com>
2020-09-20 20:26:06 +0800

    feat: add useDebounce and useThrottle

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,1 +1,17 @@
-export default function useDebounce() {}
+import { ref, watch } from 'vue-demi';
+import type { Ref } from 'vue-demi';
+import { useDebounceFn } from '..';
+
+export default function useDebounce<T>(value: Ref<T>, wait = 0) {
+  if (wait <= 0) return value;
+
+  const delayValue: Ref<T> = ref(value.value) as Ref<T>;
+
+  const updater = useDebounceFn(() => {
+    delayValue.value = value.value;
+  }, wait);
+
+  watch(value, updater);
+
+  return delayValue;
+}
--- src/hooks/useThrottle.ts
+++ src/hooks/useThrottle.ts
@@ -1,1 +1,17 @@
-export default function useThrottle() {}
+import { ref, watch } from 'vue-demi';
+import type { Ref } from 'vue-demi';
+import { useThrottleFn } from '..';
+
+export default function useThrottle<T>(value: Ref<T>, wait = 0) {
+  if (wait <= 0) return value;
+
+  const delayValue: Ref<T> = ref(value.value) as Ref<T>;
+
+  const updater = useThrottleFn(() => {
+    delayValue.value = value.value;
+  }, wait);
+
+  watch(value, updater);
+
+  return delayValue;
+}
