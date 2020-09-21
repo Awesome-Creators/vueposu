6ea4757d20a537792c2d5ae07b6b71bbc1ec717d
glitchboyl <lonelyliar8523@gmail.com>
2020-09-21 12:57:24 +0800

    test: rewritten useTitle test case

--- test/unit/hooks/useTitle.spec.ts
+++ test/unit/hooks/useTitle.spec.ts
@@ -2,67 +2,39 @@ import { mount } from '@vue/test-utils';
 import { defineComponent, ref } from 'vue-demi';
 import useTitle from '@hooks/useTitle';
 
-// TODO: remove warn
-const warn = console.warn.bind(this);
-
 describe('hooks/useTitle', () => {
-  beforeAll(() => (console.warn = () => {}));
-  afterAll(() => (console.warn = warn));
-
-  it('should be set the title and call side-effect function', async () => {
-    let useTitleCallbackTimes = 0;
-
+  it('should be set the title', async () => {
+    const defaultTitle = document.title;
     const component = mount(
       defineComponent({
-        components: {
-          Sub: {
-            setup() {
-              useTitle('sub');
-            },
-            template: `<div>i m sub</div>`,
-          },
-        },
         setup() {
-          const show = ref(true);
-          useTitle('parent', () => {
-            useTitleCallbackTimes++;
-          });
+          const title = useTitle();
 
           return {
-            show,
+            title,
             useTitle,
           };
         },
         template: `
           <div>
-            <Sub v-if="show" />
-            <button id="toggle-sub" @click="() => (show = !show)">toggle sub</button>
-            <button id="change-title" @click="useTitle('i am changed')">change title</button>
+            <button id="change-title" @click="title = 'b'">change title</button>
           </div>
         `,
       }),
       { attachTo: document.body },
     );
+    ;
 
-    expect(component.vm.show).toBe(true);
-    expect(document.title).toBe('sub');
-    expect(useTitleCallbackTimes).toBe(1);
+    expect(document.title).toBe(defaultTitle);
 
-    await component.find('#toggle-sub').trigger('click');
-    expect(component.vm.show).toBe(false);
-    expect(document.title).toBe('parent');
-    expect(useTitleCallbackTimes).toBe(2);
+    component.vm.title = 'a';
+    expect(document.title).toBe('a');
 
     await component.find('#change-title').trigger('click');
-    expect(document.title).toBe('i am changed');
+    expect(document.title).toBe('b');
 
-    await component.find('#toggle-sub').trigger('click');
-    expect(component.vm.show).toBe(true);
-    expect(document.title).toBe('sub');
-
-    component.unmount();
-
-    expect(document.title).toBe('sub');
-    expect(useTitleCallbackTimes).toBe(2);
+    expect(() => component.vm.useTitle('c')).toThrowError(
+      'Invalid hook call. `useTitle` can only be called inside of `setup()`.',
+    );
   });
 });
