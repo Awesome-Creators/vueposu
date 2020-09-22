f95782726511c4accd7a1626922ef0028c70589f
yuxino <nbsawz@gmail.com>
2020-09-22 22:21:06 +0800

    ci: add release scripts and actions

---
+++ scripts/release.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+echo "Enter new version: "
+read -r VERSION
+VERSION="v${VERSION/v/}"
+
+read -p "Tag to $VERSION - are you sure? (y/n) " -n 1 -r
+
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+
+  echo "v$VERSION ?"
+
+  npm version "$VERSION"
+
+  git add -A
+
+  git commit -m "chore: release $VERSION"
+
+  git push
+
+  git tag $VERSION
+
+  git push --tags
+fi
