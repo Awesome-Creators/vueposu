85cc1bbaed8c6d97cb2380f1b77fb724cd271e13
glitchboyl <lonelyliar8523@gmail.com>
2020-09-30 18:13:24 +0800

    feat: libs/helper add isEqual and isUndefined

--- src/libs/helper.ts
+++ src/libs/helper.ts
@@ -5,19 +5,22 @@ import {
   isFunction,
   isUndefined,
   isArray,
+  isEqual,
 } from 'lodash-es';
 
 const isDef = val => !isUndefined(val) && val !== null;
 const isUndef = val => isUndefined(val) || val === null;
-const isClient = typeof window !== 'undefined';
+const isClient = !isUndefined(typeof window);
 
 export {
-  isObject,
-  isFunction,
   debounce,
   throttle,
+  isObject,
+  isFunction,
+  isUndefined,
   isDef,
   isUndef,
   isClient,
   isArray,
+  isEqual,
 };
