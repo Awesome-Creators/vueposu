bdff2dc1492e08cae6948c3162c5cdbbee015bf8
yuxino <nbsawz@gmail.com>
2020-09-30 18:28:27 +0800

    feat(wip): add useDynamicList

--- src/hooks/useDynamicList.ts
+++ src/hooks/useDynamicList.ts
@@ -1,10 +1,14 @@
-export interface UseDynamicListActions<T> {
+import { ref } from 'vue-demi';
+import type { Ref } from 'vue-demi';
+import { isArray, isUndef } from '../libs/helper';
+
+export interface UseDynamicListActions<T extends Array<S>, S> {
   move(idx: number, idx2: number);
   insert(idx: number);
   insertBefore(idx: number);
   insertAfter(idx: number);
   getKey(idx: number);
-  delete(idx: number);
+  deleteByIdx(idx: number);
   replace(idx: number, idx2: number);
   sortList();
   filterList();
@@ -13,9 +17,14 @@ export interface UseDynamicListActions<T> {
   map();
   shift();
   pop();
+  reduce();
+  push(val: S);
 }
 
-export type UseDynamicList = [number];
+export type UseDynamicListReturnType<T extends Array<S>, S> = [
+  Ref<T>,
+  UseDynamicListActions<T, S>,
+];
 
 export interface UseDynamicListOptions {
   sort();
@@ -28,4 +37,40 @@ export interface UseDynamicListOptions {
 // insert
 // TODO: COMMENT NEED
 
-export default function useDynamicList() {}
+export default function useDynamicList<T extends Array<S>, S>(
+  initValue: T | undefined[] = [],
+): UseDynamicListReturnType<T, S> {
+  if (isArray(initValue) === false) {
+    throw new Error('initValue should be a array');
+  }
+  const state = ref(initValue) as Ref<T>;
+  const actions: UseDynamicListActions<T, S> = {
+    move(idx: number, idx2: number) {},
+    insert(idx: number) {},
+    insertBefore(idx: number) {},
+    insertAfter(idx: number) {},
+    getKey(idx: number) {},
+    deleteByIdx(idx: number) {
+      state.value.splice(idx, 1);
+    },
+    replace(idx: number, idx2: number) {},
+    sortList() {
+      return state.value.sort((a, b) => Number(a) - Number(b));
+    },
+    filterList() {},
+    resetList() {},
+    unshift() {},
+    map() {},
+    shift() {
+      state.value.shift();
+    },
+    pop() {
+      state.value.pop();
+    },
+    reduce() {},
+    push(val: S) {
+      state.value.push(val);
+    },
+  };
+  return [state, actions];
+}
--- test/unit/hooks/useDynamicList.spec.ts
+++ test/unit/hooks/useDynamicList.spec.ts
@@ -1,12 +1,60 @@
 import { mount } from '@vue/test-utils';
 import { defineComponent } from 'vue';
+import useDynamicList from '@hooks/useDynamicList';
+import { wait } from '../../utils/helper';
 
 describe('hooks/useDynamicList', () => {
-  it('test', () => {
-    // TOOD: ..
+  it('test', async () => {
+    const component = mount(
+      defineComponent({
+        template: `<template>{{ JSON.stringify(state) }}</template>`,
+        setup() {
+          const [
+            state,
+            { push, pop, shift, deleteByIdx, sortList },
+          ] = useDynamicList([1, 2, 3]);
+          return { state, push, pop, shift, deleteByIdx, sortList };
+        },
+      }),
+    );
+
+    const checkState = async array => {
+      await wait();
+      console.log(component.text());
+      expect(component.text()).toBe(JSON.stringify(array));
+    };
+
+    await wait();
+
+    await checkState([1, 2, 3]);
+
+    component.vm.push(4);
+    await checkState([1, 2, 3, 4]);
+
+    component.vm.pop();
+    await checkState([1, 2, 3]);
+
+    component.vm.shift();
+    await checkState([2, 3]);
+
+    component.vm.deleteByIdx(0);
+    await checkState([3]);
+
+    component.vm.push(2);
+    expect(component.vm.sortList()).toEqual([2, 3]);
+  });
+
+  it('test initValue not array', () => {
     mount(
       defineComponent({
         template: `<template />`,
+        setup() {
+          try {
+            expect(useDynamicList(1 as any)).toThrowError(
+              'initValue should be a array',
+            );
+          } catch {}
+        },
       }),
     );
   });
