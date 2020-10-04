f81a16ffe0b8da01efebbfed4d000a983d5c5f56
glitchboyl <lonelyliar8523@gmail.com>
2020-10-05 00:15:33 +0800

    chore: modify imports script

--- scripts/imports.js
+++ scripts/imports.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 
 const hooks = fs
   .readdirSync(__dirname + '/../src/hooks')
-  .filter(file => file.indexOf('.ts') !== -1)
   .map(hook => hook.replace('.ts', ''));
 
 fs.writeFileSync(
