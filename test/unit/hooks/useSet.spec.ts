f18d02b22086ca3eb81411c463cef480f51c09eb
glitchboyl <lonelyliar8523@gmail.com>
2020-10-14 17:44:32 +0800

    test: change useSet return type to object

--- test/unit/hooks/useSet.spec.ts
+++ test/unit/hooks/useSet.spec.ts
@@ -3,21 +3,19 @@ import { mount } from '@vue/test-utils';
 import { defineComponent } from 'vue-demi';
 
 function setUp<K>(initialSet?: Set<K>) {
-  const component = mount(
+  return mount(
     defineComponent({
       template: `<template />`,
       setup: () => ({
-        set: useSet(initialSet),
+        set: useSet(initialSet)
       }),
     }),
   );
-  return component;
 }
 
 describe('hooks/useSet', () => {
   it('should init set and utils', () => {
-    const { vm } = setUp(new Set([1, 2]));
-    const [set, utils] = vm.set;
+    const { set, ...utils } = setUp(new Set([1, 2])).vm.set;
 
     expect(set.value).toEqual(new Set([1, 2]));
     expect(utils).toStrictEqual({
@@ -29,100 +27,65 @@ describe('hooks/useSet', () => {
   });
 
   it('should init empty set if no initial set provided', () => {
-    const { vm } = setUp();
+    const { set } = setUp().vm.set;
 
-    expect(vm.set[0].value).toEqual(new Set());
+    expect(set.value).toEqual(new Set());
   });
 
   it('should have an initially provided key', () => {
-    const { vm } = setUp(new Set(['a']));
-    const [, utils] = vm.set;
+    const { has } = setUp(new Set(['a'])).vm.set;
 
-    let value;
-    value = utils.has('a');
-
-    expect(value).toBe(true);
+    expect(has('a')).toBe(true);
   });
 
   it('should have an added key', () => {
-    const { vm } = setUp(new Set());
+    const { add, has } = setUp(new Set()).vm.set;
 
-    vm.set[1].add('newKey');
-
-    let value;
-    value = vm.set[1].has('newKey');
-
-    expect(value).toBe(true);
+    add('newKey');
+    expect(has('newKey')).toBe(true);
   });
 
   it('should get false for non-existing key', () => {
-    const { vm } = setUp(new Set(['a']));
-    const [, utils] = vm.set;
+    const { has } = setUp(new Set(['a'])).vm.set;
 
-    let value;
-    value = utils.has('nonExisting');
-
-    expect(value).toBe(false);
+    expect(has('nonExisting')).toBe(false);
   });
 
   it('should add a new key', () => {
-    const { vm } = setUp(new Set(['oldKey']));
-    const [, utils] = vm.set;
+    const { set, add } = setUp(new Set(['oldKey'])).vm.set;
 
-    utils.add('newKey');
-
-    expect(vm.set[0].value).toEqual(new Set(['oldKey', 'newKey']));
+    add('newKey');
+    expect(set.value).toEqual(new Set(['oldKey', 'newKey']));
   });
 
   it('should work if setting existing key', () => {
-    const { vm } = setUp(new Set(['oldKey']));
-    const [, utils] = vm.set;
+    const { set, add } = setUp(new Set(['oldKey'])).vm.set;
 
-    utils.add('oldKey');
-
-    expect(vm.set[0].value).toEqual(new Set(['oldKey']));
+    add('oldKey');
+    expect(set.value).toEqual(new Set(['oldKey']));
   });
 
   it('should remove existing key', () => {
-    const { vm } = setUp(new Set([1, 2]));
-    const [, utils] = vm.set;
+    const { set, remove } = setUp(new Set([1, 2])).vm.set;
 
-    utils.remove(2);
-
-    expect(vm.set[0].value).toEqual(new Set([1]));
+    remove(2);
+    expect(set.value).toEqual(new Set([1]));
   });
 
   it('should do nothing if removing non-existing key', () => {
-    const { vm } = setUp(new Set(['a', 'b']));
-    const [, utils] = vm.set;
+    const { set, remove } = setUp(new Set(['a', 'b'])).vm.set;
 
-    utils.remove('nonExisting');
-
-    expect(vm.set[0].value).toEqual(new Set(['a', 'b']));
+    remove('nonExisting');
+    expect(set.value).toEqual(new Set(['a', 'b']));
   });
 
   it('should reset to initial set provided', () => {
-    const { vm } = setUp(new Set([1]));
-    const [, utils] = vm.set;
+    const { set, add, reset } = setUp(new Set([1])).vm.set;
 
-    utils.add(2);
+    add(2);
+    expect(set.value).toEqual(new Set([1, 2]));
 
-    expect(vm.set[0].value).toEqual(new Set([1, 2]));
-
-    utils.reset();
-
-    expect(vm.set[0].value).toEqual(new Set([1]));
-  });
-
-  it('should memoized its utils methods', () => {
-    const { vm } = setUp(new Set(['a', 'b']));
-    const [, utils] = vm.set;
-    const { add, remove, reset } = utils;
-
-    add('foo');
-
-    expect(vm.set[1].add).toBe(add);
-    expect(vm.set[1].remove).toBe(remove);
-    expect(vm.set[1].reset).toBe(reset);
+    reset();
+    expect(set.value).toEqual(new Set([1]));
   });
 });
