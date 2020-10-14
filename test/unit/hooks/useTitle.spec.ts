9d06ec24ad959b3d9f0fba8fbe01152dcca69b04
glitchboyl <lonelyliar8523@gmail.com>
2020-10-14 18:27:41 +0800

    test: modify useTitle test cases

--- test/unit/hooks/useTitle.spec.ts
+++ test/unit/hooks/useTitle.spec.ts
@@ -4,26 +4,62 @@ import useTitle from '@hooks/useTitle';
 import { wait } from '@test/utils/helper';
 
 describe('hooks/useTitle', () => {
-  it('should be set the title', async () => {
-    const defaultTitle = document.title;
-    const component = mount(
-      defineComponent({
-        setup() {
-          const title = useTitle();
+  const $mutationObserver = window.MutationObserver;
+  const constructorFn = jest.fn();
+  const observeFn = jest.fn();
+  const disconnectFn = jest.fn();
+  class MutationObserver {
+    observer: any;
+    constructor(callback: MutationCallback) {
+      this.observer = new $mutationObserver(callback);
+      constructorFn();
+    }
+    observe = (...args) => {
+      this.observer.observe(...args);
+      observeFn();
+    };
+    disconnect = (...args) => {
+      this.observer.disconnect(...args);
+      disconnectFn();
+    };
+  }
+  Object.defineProperty(window, 'MutationObserver', {
+    writable: true,
+    configurable: true,
+    value: MutationObserver,
+  });
+  Object.defineProperty(global, 'MutationObserver', {
+    writable: true,
+    configurable: true,
+    value: MutationObserver,
+  });
 
-          return {
-            title,
-            useTitle,
-          };
-        },
-        template: `
+  const defaultTitle = document.title;
+  const component = mount(
+    defineComponent({
+      setup() {
+        const title = useTitle();
+
+        return {
+          title,
+          useTitle,
+        };
+      },
+      template: `
           <div>
             <button id="change-title" @click="title = 'b'">change title</button>
           </div>
         `,
-      }),
-      { attachTo: document.body },
-    );
+    }),
+    { attachTo: document.body },
+  );
+
+  it('should observe the `document.title`', () => {
+    expect(constructorFn).toHaveBeenCalled();
+    expect(observeFn).toHaveBeenCalled();
+  });
+
+  it('should set title', async () => {
     expect(document.title).toBe(defaultTitle);
 
     component.vm.title = 'a';
@@ -31,14 +67,24 @@ describe('hooks/useTitle', () => {
 
     await component.find('#change-title').trigger('click');
     expect(document.title).toBe('b');
+  });
 
-    // MutationObserver
+  it('should be updated if the `document.title` changes', async () => {
     document.title = 'c';
     await wait();
     expect(component.vm.title).toBe('c');
+  });
 
+  it('should throw error when `useTitle` not be called inside of `setup()`', () => {
     expect(() => component.vm.useTitle('d')).toThrowError(
       'Invalid hook call: `useTitle` can only be called inside of `setup()`.',
     );
+  });
+
+  it('should disconnect observe on unmount', () => {
+    expect(disconnectFn).not.toHaveBeenCalled();
+
+    component.unmount();
+    expect(disconnectFn).toHaveBeenCalled();
   });
 });
