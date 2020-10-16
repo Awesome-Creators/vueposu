3a833c8f3266b8b80c8e634c9ff7fd08a7144655
glitchboyl <lonelyliar8523@gmail.com>
2020-10-16 16:32:11 +0800

    test: useTitle add optional param 'restoreOnUnmount' & method 'restoreTitle'

--- test/unit/hooks/useTitle.spec.ts
+++ test/unit/hooks/useTitle.spec.ts
@@ -8,83 +8,132 @@ describe('hooks/useTitle', () => {
   const constructorFn = jest.fn();
   const observeFn = jest.fn();
   const disconnectFn = jest.fn();
-  class MutationObserver {
-    observer: any;
-    constructor(callback: MutationCallback) {
-      this.observer = new $mutationObserver(callback);
-      constructorFn();
+
+  const mockMutationObserver = () => {
+    class MutationObserver {
+      constructor() {
+        constructorFn();
+      }
+      observe = observeFn;
+      disconnect = disconnectFn;
     }
-    observe = (...args) => {
-      this.observer.observe(...args);
-      observeFn();
-    };
-    disconnect = (...args) => {
-      this.observer.disconnect(...args);
-      disconnectFn();
-    };
-  }
-  Object.defineProperty(window, 'MutationObserver', {
-    writable: true,
-    configurable: true,
-    value: MutationObserver,
-  });
-  Object.defineProperty(global, 'MutationObserver', {
-    writable: true,
-    configurable: true,
-    value: MutationObserver,
-  });
+    Object.defineProperty(window, 'MutationObserver', {
+      writable: true,
+      configurable: true,
+      value: MutationObserver,
+    });
+    Object.defineProperty(global, 'MutationObserver', {
+      writable: true,
+      configurable: true,
+      value: MutationObserver,
+    });
+  };
 
-  const defaultTitle = document.title;
-  const component = mount(
-    defineComponent({
-      setup() {
-        const title = useTitle();
+  const restoreMutationObserver = () => {
+    Object.defineProperty(window, 'MutationObserver', {
+      writable: true,
+      configurable: true,
+      value: $mutationObserver,
+    });
+    Object.defineProperty(global, 'MutationObserver', {
+      writable: true,
+      configurable: true,
+      value: $mutationObserver,
+    });
+  };
 
-        return {
-          title,
-          useTitle,
-        };
-      },
-      template: `
-          <div>
-            <button id="change-title" @click="title = 'b'">change title</button>
-          </div>
-        `,
-    }),
-    { attachTo: document.body },
-  );
+  const originalTitle = document.title;
+  const getComponent = (...args: Parameters<typeof useTitle>) =>
+    mount(
+      defineComponent({
+        setup() {
+          return useTitle(...args);
+        },
+        template: `<div></div>`,
+      }),
+    );
+
+  beforeEach(() => {
+    restoreMutationObserver();
+  });
 
   it('should observe the `document.title`', () => {
+    mockMutationObserver();
+    getComponent();
+
     expect(constructorFn).toHaveBeenCalled();
     expect(observeFn).toHaveBeenCalled();
   });
 
-  it('should set title', async () => {
-    expect(document.title).toBe(defaultTitle);
+  it('should set the title when change `title` ref value', () => {
+    const component = getComponent();
+
+    expect(document.title).toBe(originalTitle);
 
     component.vm.title = 'a';
     expect(document.title).toBe('a');
 
-    await component.find('#change-title').trigger('click');
+    component.unmount();
+  });
+
+  it('should override the current title', () => {
+    const component = getComponent('b');
+
+    expect(component.vm.title).toBe('b');
     expect(document.title).toBe('b');
+
+    component.unmount();
+  });
+
+  it('should restore the title', () => {
+    const component = getComponent('c');
+
+    expect(component.vm.title).toBe('c');
+    expect(document.title).toBe('c');
+
+    component.vm.restoreTitle();
+    expect(component.vm.title).toBe(originalTitle);
+    expect(document.title).toBe(originalTitle);
+
+    component.unmount();
   });
 
   it('should be updated if the `document.title` changes', async () => {
-    document.title = 'c';
+    const component = getComponent();
+
+    expect(component.vm.title).toBe(originalTitle);
+    expect(document.title).toBe(originalTitle);
+
+    document.title = 'd';
     await wait();
-    expect(component.vm.title).toBe('c');
+    expect(component.vm.title).toBe('d');
+
+    component.unmount();
   });
 
-  it('should throw error when `useTitle` not be called inside of `setup()`', () => {
-    expect(() => component.vm.useTitle('d')).toThrowError(
-      'Invalid hook call: `useTitle` can only be called inside of `setup()`.',
-    );
-  });
+  it('should restore the title and disconnect observe on unmount', () => {
+    mockMutationObserver();
+    const component = getComponent('f');
 
-  it('should disconnect observe on unmount', () => {
     expect(disconnectFn).not.toHaveBeenCalled();
 
     component.unmount();
     expect(disconnectFn).toHaveBeenCalled();
+  });
+
+  it("should not restore the title on unmount when param `restoreOnUnmount` is `false`", () => {
+    const component = getComponent('e', false);
+
+    expect(component.vm.title).toBe('e');
+    expect(document.title).toBe('e');
+
+    component.unmount();
+    expect(document.title).toBe('e');
+  });
+
+  it('should throw error when `useTitle` not be called inside of `setup()`', () => {
+    expect(() => useTitle('g')).toThrowError(
+      'Invalid hook call: `useTitle` can only be called inside of `setup()`.',
+    );
   });
 });
