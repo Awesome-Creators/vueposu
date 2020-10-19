9e5f6c88fc6c87f82d13db886f07f10c2c4af71a
glitchboyl <lonelyliar8523@gmail.com>
2020-10-19 11:26:55 +0800

    feat: useEventEmitter

--- src/hooks/useEventEmitter.ts
+++ src/hooks/useEventEmitter.ts
@@ -1,1 +1,20 @@
-export default function () {}
+type Subscription = (v: any) => void;
+
+export class EventEmitter {
+  #subscriptions = new Set<Subscription>();
+
+  emit = (v: any) => {
+    for (let subscription of this.#subscriptions) {
+      subscription(v);
+    }
+  };
+
+  useSubscription = (s: Subscription) => {
+    this.#subscriptions.add(s);
+  };
+}
+
+// TODO: COMMENT NEED
+export default function useEventEmitter() {
+  return new EventEmitter();
+}
