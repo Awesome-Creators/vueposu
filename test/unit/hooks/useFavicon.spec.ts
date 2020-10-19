a81df873124c97384395e31a91b644881f3272a9
glitchboyl <lonelyliar8523@gmail.com>
2020-10-19 15:17:59 +0800

    test: useFavicon restore icon and change return type to object

--- test/unit/hooks/useFavicon.spec.ts
+++ test/unit/hooks/useFavicon.spec.ts
@@ -3,50 +3,66 @@ import { mount } from '@vue/test-utils';
 import { defineComponent } from 'vue-demi';
 
 describe('hooks/useFavicon', () => {
-  it('test useFavicon', async () => {
-    const component = mount(
+  const icon =
+    (document.querySelector("link[rel*='icon']") as HTMLLinkElement) ||
+    (() => {
+      const link = document.createElement('link');
+      link.type = 'image/x-icon';
+      link.rel = 'shortcut icon';
+      document.getElementsByTagName('head')[0].appendChild(link);
+      return link;
+    })();
+
+  it('should not set the favicon when param `url` is undef', () => {
+    mount(
       defineComponent({
         template: `<template />`,
-        setup: () => ({ useFavicon: useFavicon() }),
+        setup: () => useFavicon(),
       }),
     );
 
-    expect(document.querySelector("link[rel*='icon']") as HTMLLinkElement).toBe(
-      null,
-    );
-
-    component.unmount();
+    expect(icon.href).toBe('');
   });
 
-  it('test useFavicon', async () => {
-    const component = mount(
+  it('should set the favicon when param `url` is def', () => {
+    mount(
       defineComponent({
-        setup: () => ({
-          changeIcon: useFavicon('https://www.test.com/icon.ico')[0],
-        }),
+        setup: () => useFavicon('https://test.com/icon.ico'),
         template: `<template />`,
       }),
     );
 
-    expect(
-      (document.querySelector("link[rel*='icon']") as HTMLLinkElement).href,
-    ).toBe('https://www.test.com/icon.ico');
+    expect(icon.href).toBe('https://test.com/icon.ico');
+  });
 
-    component.vm.changeIcon('https://www.another.com/icon.ico');
-    expect(
-      (document.querySelector("link[rel*='icon']") as HTMLLinkElement).href,
-    ).toBe('https://www.another.com/icon.ico');
+  it('should can change the favicon and restore the favicon', () => {
+    icon.href = 'https://favicon.com/icon.ico';
+
+    const component = mount(
+      defineComponent({
+        setup: () => useFavicon(),
+        template: `<template />`,
+      }),
+    );
+
+    expect(icon.href).toBe('https://favicon.com/icon.ico');
+
+    component.vm.changeIcon('https://another.com/icon.ico');
+    expect(icon.href).toBe('https://another.com/icon.ico');
 
     component.vm.changeIcon('1');
-    expect(
-      (document.querySelector("link[rel*='icon']") as HTMLLinkElement).href,
-    ).toBe('http://localhost/1');
+    expect(icon.href).toBe('http://localhost/1');
 
     component.vm.changeIcon('');
-    expect(
-      (document.querySelector("link[rel*='icon']") as HTMLLinkElement).href,
-    ).toBe('http://localhost/1');
+    expect(icon.href).toBe('http://localhost/1');
 
-    component.unmount();
+    component.vm.restoreIcon();
+    expect(icon.href).toBe('https://favicon.com/icon.ico');
+  });
+
+  it('should throw error when `useFavicon` not be called inside of `setup()`', () => {
+    expect(() => useFavicon()).toThrowError(
+      'Invalid hook call: `useFavicon` can only be called inside of `setup()`.',
+    );
   });
 });
