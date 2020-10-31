f692993965e9e6d5735e81b60f403f802073fe55
glitchboyl <lonelyliar8523@gmail.com>
2020-10-31 22:34:17 +0800

    chore: modify useQueue type

--- src/hooks/useQueue.ts
+++ src/hooks/useQueue.ts
@@ -12,7 +12,7 @@ export interface QueueMethodsReturnType<T> {
 }
 
 const useQueue = <T>(initialValue: T[] = []): QueueMethodsReturnType<T> => {
-  const state = ref(Array.from(initialValue));
+  const state = ref<T[]>(Array.from(initialValue));
   return {
     add: value => state.value.push(value as any),
     remove: () => state.value.shift() as any,
@@ -20,13 +20,13 @@ const useQueue = <T>(initialValue: T[] = []): QueueMethodsReturnType<T> => {
       state.value = [];
     },
     reset: () => {
-      state.value = initialValue as any;
+      state.value = Array.from(initialValue) as any;
     },
     get first() {
-      return state.value[0] as any;
+      return state.value[0] as T;
     },
     get last() {
-      return state.value[state.value.length - 1] as any;
+      return state.value[state.value.length - 1] as T;
     },
     get size() {
       return state.value.length;
