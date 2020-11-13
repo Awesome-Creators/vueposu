3516a0c218b7622ea691abfbdd4942c0ac22499c
glitchboyl <lonelyliar8523@gmail.com>
2020-11-13 15:56:19 +0800

    refactor(wip): useEventEmitter

--- src/hooks/useEventEmitter.ts
+++ src/hooks/useEventEmitter.ts
@@ -1,25 +1,40 @@
-import { getCurrentInstance } from 'vue-demi';
+import { ref, watch, onUnmounted, getCurrentInstance } from 'vue-demi';
+import { isFunction } from '../libs/helper';
 
-type Subscription = (v: any) => void;
-
-export class EventEmitter {
-  #subscriptions = new Set<Subscription>();
-
-  emit = (v: any) => {
-    for (let subscription of this.#subscriptions) {
-      subscription(v);
-    }
-  };
-
-  useSubscription = (s: Subscription) => {
-    this.#subscriptions.add(s);
-  };
-}
+type Listener = (...stream) => void;
 
 // TODO: COMMENT NEED
 export default function useEventEmitter() {
   if (getCurrentInstance()) {
-    return new EventEmitter();
+    const emitID = ref(0);
+    const stream = ref([]);
+
+    const subscriptions = new Set<Listener>();
+
+    const emit = (...args) => {
+      emitID.value++;
+      stream.value = args;
+    };
+
+    const on = (listener: Listener) => {
+      if (isFunction(listener)) {
+        subscriptions.add(listener);
+      }
+    };
+
+    watch(emitID, () => {
+      for (let listener of subscriptions) {
+        listener(...stream.value);
+      }
+    }, {
+      flush: 'post'
+    });
+
+    onUnmounted(() => {
+      subscriptions.clear();
+    });
+
+    return { emit, on };
   } else {
     throw new Error(
       'Invalid hook call: `useEventEmitter` can only be called inside of `setup()`.',
