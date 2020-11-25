16ed88d531abd823cb45b1c6198f6e0625204ded
glitchboyl <lonelyliar8523@gmail.com>
2020-11-25 16:40:13 +0800

    feat: useSet can clear all keys

--- src/hooks/useSet.ts
+++ src/hooks/useSet.ts
@@ -1,37 +1,50 @@
-import { readonly, ref, unref } from 'vue-demi';
+import { readonly, ref, unref, getCurrentInstance } from 'vue-demi';
 
 import type { DeepReadonly, Ref } from 'vue-demi';
+import type { RefTyped } from '../types/global';
 
-// TODO: COMMENT NEED
-export interface SetActions<K> {
+interface SetActions<K> {
   add: (key: K) => void;
   remove: (key: K) => void;
-  reset: () => void;
   has: (key: K) => boolean;
+  reset: () => void;
+  clear: () => void;
 }
 
 type UseSetReturnType<K> = {
   set: DeepReadonly<Ref<Set<K>>>;
 } & SetActions<K>;
 
-const useSet = <K>(initialSet = new Set<K>()): UseSetReturnType<K> => {
+// TODO: COMMENT NEED
+const useSet = <K = any>(
+  initialSet?: RefTyped<Iterable<K>>,
+): UseSetReturnType<K> => {
+  if (!getCurrentInstance()) {
+    throw new Error(
+      'Invalid hook call: `useSet` can only be called inside of `setup()`.',
+    );
+  }
+
   const set = ref<Set<K>>(new Set(unref(initialSet)));
-  const setActions = {
+  const actions = {
     add: (item: K) => {
       set.value.add(item);
     },
     remove: (item: K) => {
       set.value.delete(item);
     },
-    reset: () => {
-      set.value = new Set(initialSet);
-    },
     has: (item: K) => set.value.has(item),
+    reset: () => {
+      set.value = new Set(unref(initialSet));
+    },
+    clear: () => {
+      set.value.clear();
+    },
   };
 
   return {
     set: readonly(set),
-    ...setActions,
+    ...actions,
   };
 };
 
--- test/unit/hooks/useSet.spec.ts
+++ test/unit/hooks/useSet.spec.ts
@@ -2,90 +2,106 @@ import useSet from '@hooks/useSet';
 import { mount } from '@vue/test-utils';
 import { defineComponent } from 'vue-demi';
 
-function setUp<K>(initialSet?: Set<K>) {
-  return mount(
+const setup = (initialSet?: any) =>
+  mount(
     defineComponent({
       template: `<template />`,
       setup: () => ({
-        set: useSet(initialSet)
+        set: useSet(initialSet),
       }),
     }),
   );
-}
 
 describe('hooks/useSet', () => {
   it('should init set and utils', () => {
-    const { set, ...utils } = setUp(new Set([1, 2])).vm.set;
+    const { set, ...utils } = setup(new Set([1, 2])).vm.set;
 
     expect(set.value).toEqual(new Set([1, 2]));
     expect(utils).toStrictEqual({
-      has: expect.any(Function),
       add: expect.any(Function),
       remove: expect.any(Function),
+      has: expect.any(Function),
       reset: expect.any(Function),
+      clear: expect.any(Function),
     });
   });
 
   it('should init empty set if no initial set provided', () => {
-    const { set } = setUp().vm.set;
+    const { set } = setup().vm.set;
 
     expect(set.value).toEqual(new Set());
   });
 
   it('should have an initially provided key', () => {
-    const { has } = setUp(new Set(['a'])).vm.set;
+    const { has } = setup(new Set(['a'])).vm.set;
 
     expect(has('a')).toBe(true);
   });
 
   it('should have an added key', () => {
-    const { add, has } = setUp(new Set()).vm.set;
+    const { add, has } = setup(new Set()).vm.set;
 
     add('newKey');
     expect(has('newKey')).toBe(true);
   });
 
   it('should get false for non-existing key', () => {
-    const { has } = setUp(new Set(['a'])).vm.set;
+    const { has } = setup(new Set(['a'])).vm.set;
 
     expect(has('nonExisting')).toBe(false);
   });
 
   it('should add a new key', () => {
-    const { set, add } = setUp(new Set(['oldKey'])).vm.set;
+    const { set, add } = setup(new Set(['oldKey'])).vm.set;
 
     add('newKey');
     expect(set.value).toEqual(new Set(['oldKey', 'newKey']));
   });
 
   it('should work if setting existing key', () => {
-    const { set, add } = setUp(new Set(['oldKey'])).vm.set;
+    const { set, add } = setup(new Set(['oldKey'])).vm.set;
 
     add('oldKey');
     expect(set.value).toEqual(new Set(['oldKey']));
   });
 
   it('should remove existing key', () => {
-    const { set, remove } = setUp(new Set([1, 2])).vm.set;
+    const { set, remove } = setup(new Set([1, 2])).vm.set;
 
     remove(2);
     expect(set.value).toEqual(new Set([1]));
   });
 
   it('should do nothing if removing non-existing key', () => {
-    const { set, remove } = setUp(new Set(['a', 'b'])).vm.set;
+    const { set, remove } = setup(new Set(['a', 'b'])).vm.set;
 
     remove('nonExisting');
     expect(set.value).toEqual(new Set(['a', 'b']));
   });
 
   it('should reset to initial set provided', () => {
-    const { set, add, reset } = setUp(new Set([1])).vm.set;
+    const { set, add, reset } = setup(new Set([1])).vm.set;
 
     add(2);
     expect(set.value).toEqual(new Set([1, 2]));
 
     reset();
     expect(set.value).toEqual(new Set([1]));
+  });
+
+  it('should clear all keys', () => {
+    const { set, add, clear } = setup(new Set([1, 2])).vm.set;
+
+    add(3);
+    expect(set.value).toEqual(new Set([1, 2, 3]));
+
+    clear();
+    expect(set.value).toEqual(new Set([]));
+  });
+
+  it('should throw error when `useSet` not be called inside of `setup()`', () => {
+    expect(() => useSet()).toThrowError(
+      'Invalid hook call: `useSet` can only be called inside of `setup()`.',
+    );
   });
 });
