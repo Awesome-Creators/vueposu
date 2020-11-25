6e280f92c20824d805359c62375cb6273bedd053
glitchboyl <lonelyliar8523@gmail.com>
2020-11-25 16:41:05 +0800

    test: useEventEmitter should unsubscribe all emitters when unmounted

--- test/unit/hooks/useEventEmitter.spec.ts
+++ test/unit/hooks/useEventEmitter.spec.ts
@@ -56,6 +56,44 @@ describe('hooks/useEventEmitter', () => {
     expect(component.vm.text).toBe('dd');
   });
 
+  it('should unsubscribe all emitters when unmounted', async () => {
+    const fn = jest.fn();
+    const originalSetClear = Set.prototype.clear;
+    Set.prototype.clear = function () {
+      fn();
+      originalSetClear.bind(this)();
+    };
+    const component = mount(
+      defineComponent({
+        template: '<template />',
+        setup() {
+          const count = ref(0);
+          const { emit, on } = useEventEmitter();
+
+          on(() => {
+            count.value++;
+          });
+
+          return {
+            count,
+            emit,
+            on,
+          };
+        },
+      }),
+    );
+    component.vm.emit();
+    await wait();
+    expect(component.vm.count).toBe(1);
+
+    component.unmount();
+    
+    component.vm.emit();
+    await wait();
+    expect(component.vm.count).toBe(1);
+    expect(fn).toBeCalledTimes(1);
+  });
+
   it('should throw error when `useEventEmitter` not be called inside of `setup()`', () => {
     expect(() => useEventEmitter()).toThrowError(
       'Invalid hook call: `useEventEmitter` can only be called inside of `setup()`.',
