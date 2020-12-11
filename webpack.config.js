12aacd4080187cdb77b5acc3ffdeff5c41955241
glitchboyl <lonelyliar8523@gmail.com>
2020-12-11 20:13:59 +0800

    refactor(wip): try to use webpack 5 to build

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+	entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+	resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+	},
+	output: {
+    path: path.resolve(__dirname, 'dist'),
+		filename: 'index.js',
+		library: 'Vueposu',
+		libraryTarget: 'umd'
+	},
+	cache: {
+    // 1. Set cache type to filesystem
+    type: 'filesystem',
+
+    buildDependencies: {
+      // 2. Add your config as buildDependency to get cache invalidation on config change
+      config: [__filename]
+
+      // 3. If you have other things the build depends on you can add them here
+      // Note that webpack, loaders and all modules referenced from your config are automatically added
+    }
+  }
+}
