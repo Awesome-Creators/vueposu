35d8b0a591d19bcc2f0761c0e48c0f411f281e72
glitchboyl <lonelyliar8523@gmail.com>
2020-12-19 18:24:36 +0800

    chore: remove ignore src/index

---
+++ packages/shared/utils/index.ts
@@ -0,0 +1,13 @@
+export * from './helper';
+export * from './math';
+
+export * from './debounce';
+export * from './throttle';
+
+export * from './dom';
+export * from './fullscreen';
+export * from './emitter';
+
+export * from './hash';
+export * from './isDocumentVisible';
+export * from './isOnline';
---
+++ packages/vueposu/src/index.ts
@@ -0,0 +1,43 @@
+// DOM
+export * from './useClickAway';
+export * from './useEventListener';
+export * from './useFavicon';
+export * from './useFullscreen';
+export * from './usePageHidden';
+export * from './useTitle';
+
+// SENSORS
+export * from './useMouse';
+export * from './useScroll';
+
+// STATE
+export * from './useCounter';
+export * from './useCounterInterval';
+export * from './useDynamicList';
+export * from './useQueue';
+export * from './useSet';
+export * from './useToggle';
+
+// SWR
+export * from './useSWR';
+
+// CACHE
+export * from './useLocalStorage';
+export * from './useSessionStorage';
+export * from './useStorage';
+
+// SIDE EFFECT
+export * from './useDebounce';
+export * from './useDebounceEffect';
+export * from './useDebounceFn';
+export * from './useEventEmitter';
+export * from './useRequest';
+export * from './useThrottle';
+export * from './useThrottleEffect';
+export * from './useThrottleFn';
+
+// ANIMATION
+export * from './useInterval';
+export * from './useTimeout';
+
+export * from '@vueposu/shared'
